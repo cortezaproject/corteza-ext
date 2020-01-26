5723248ee6b2dc2a4c2e513b0000981887bad26e
Tomaž Jerman <tomaz.jerman@kendu.si>
2020-01-26 13:07:54 +0100

    Define a basic set of state management types

---
+++ src/twilio/corredor/components/Dialpad/lib/types.js
@@ -0,0 +1,17 @@
+/**
+ * Defines the call's kind -- inbound, outbound, ...
+ */
+export const callKinds = {
+  inbound: 'inbound',
+  outbound: 'outbound',
+}
+
+/**
+ * Defines the state of the call -- ringing, inProgress, ...
+ */
+export const callStates = {
+  inactive: 'inactive',
+  pending: 'pending',
+  inProgress: 'inProgress',
+  ended: 'ended',
+}
