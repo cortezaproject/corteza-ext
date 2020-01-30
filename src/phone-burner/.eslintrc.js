dd3692d915f3728f7092a37d588447cf77c6de76
Tomaž Jerman <tomaz.jerman@kendu.si>
2020-01-30 15:43:50 +0100

    Init project

---
+++ src/phone-burner/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: false,
+  env: {
+    node: true,
+    es6: true,
+    mocha: true,
+  },
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+    '@vue/standard',
+  ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/no-v-html': 'off',
+    // @todo remove this asap - add enough tests first
+    'vue/name-property-casing': 'off',
+    'vue/prop-name-casing': 'off',
+
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+  },
+}
---
+++ src/phone-burner/.mocharc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  require: [
+    'esm',
+    'source-map-support/register',
+  ],
+  'full-trace': true,
+  bail: true,
+  recursive: true,
+  extension: ['.test.js'],
+}
