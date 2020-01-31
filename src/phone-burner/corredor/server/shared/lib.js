e489d7da6b25437b8d00d201440828815500fc61
Tomaž Jerman <tomaz.jerman@kendu.si>
2020-01-31 13:37:00 +0100

    Add a request parser helper

--- src/phone-burner/corredor/server/shared/lib.js
+++ src/phone-burner/corredor/server/shared/lib.js
@@ -1,3 +1,5 @@
+import b64 from 'base-64'
+
 /**
  * inSet function creates a query string that represents an in set check
  * @param {String} key Field name
@@ -6,4 +8,18 @@
  */
 export function inSet (key, values) {
   return values.reduce((acc, v) => acc.concat(`${key}=${v}`), []).join(' OR ')
+}
+
+/**
+ * Helper function parses request's rawBody and creates a body
+ * @param {Object} req Request object
+ * @returns {Object}
+ */
+export function parseBody (req) {
+  if (!req.rawBody) {
+    req.body = {}
+  } else {
+    req.body = JSON.parse(b64.decode(req.rawBody))
+  }
+  return req
 }
