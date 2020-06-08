04570dcc5adf36228195dc26bf7b2f2fd4ac6049
Jože Fortun <joze.fortun@gmail.com>
2020-06-08 12:39:49 +0200

    Rework UpdateEsigStatus to manual

--- docusign/server-scripts/docusign/UpdateESigStatus.js
+++ docusign/server-scripts/docusign/UpdateESigStatus.js
@@ -7,15 +7,10 @@
 //   label: 'Update E-Signature Status',
 //   description: 'Updates the status of the E-Signature',
 
-//   triggers (t) {
-//     return [
-//       t.every('0 * * * *')
-//         .for('compose'),
-
-//       t.on('manual')
-//         .for('compose:record')
-//         .uiProp('app', 'compose')
-//     ]
+//   * triggers ({ on }) {
+//     yield on('manual')
+//       .for('compose:record')
+//       .uiProp('app', 'compose')
 //   },
 
 //   async attachFile (ns, mod, fd, Compose) {
@@ -39,47 +34,37 @@
 //   },
 
 //   async exec ({ $record, $module, $namespace }, { Compose }) {
-//     let records = []
-//     if (!$record) {
-//       $namespace = await Compose.resolveNamespace('crm')
-//       $module = await Compose.findModuleByName('Quote', $namespace)
-//       const { set = [] } = await Compose.findRecords('', $module)
-//       records = set
-//     } else {
-//       records = [$record]
-//     }
-
 //     const cfg = await loadCreds(Compose)
 //     const client = new DocuSignClient(cfg.AccessToken, cfg.AccountID, cfg.InProduction)
 
-//     records.forEach(async record => {
-//       if (record.values.DocuSignId && record.values.SignatureStatus !== 'completed') {
-//         const status = await client.GetSignatureStatus(record.values.DocuSignId)
+//     if ($record.values.DocuSignId) {
+//       const status = await client.GetSignatureStatus($record.values.DocuSignId)
 
-//         if (status === 'completed') {
-//           if (record.values.OpportunityId) {
-//             await Compose.findRecordByID(record.values.OpportunityId, 'Opportunity')
-//               .then(async opportunityRecord => {
-//                 opportunityRecord.values.IsClosed = true
-//                 opportunityRecord.values.IsWon = true
-//                 await Compose.saveRecord(opportunityRecord)
-//               })
-//           }
-
-//           await client.GetDocument(record.values.DocuSignId)
-//             .then(async res => {
-//               const fd = new FormData()
-//               fd.append('upload', Buffer.from(res, 'binary'), {
-//                 filename: `${record.values.QuoteNumber}_signed.pdf`,
-//                 contentType: 'application/pdf'
-//               })
-//               record.values.QuoteFile = await this.attachFile($namespace, $module, fd, Compose)
+//       if (status === 'completed') {
+//         if ($record.values.OpportunityId) {
+//           await Compose.findRecordByID($record.values.OpportunityId, 'Opportunity')
+//             .then(async opportunityRecord => {
+//               opportunityRecord.values.IsClosed = true
+//               opportunityRecord.values.IsWon = true
+//               await Compose.saveRecord(opportunityRecord)
 //             })
 //         }
 
-//         record.values.SignatureStatus = status
-//         await Compose.saveRecord(record)
+//         await client.GetDocument($record.values.DocuSignId)
+//           .then(async res => {
+//             const fd = new FormData()
+//             fd.append('upload', Buffer.from(res, 'binary'), {
+//               filename: `${$record.values.QuoteNumber}_signed.pdf`,
+//               contentType: 'application/pdf'
+//             })
+//             $record.values.QuoteFile = await this.attachFile($namespace, $module, fd, Compose)
+//           })
 //       }
-//     })
+
+//       $record.values.SignatureStatus = status
+//       return Compose.saveRecord($record)
+//     } else {
+//       throw new Error('Cannot update E-Signature status for unexisting document')
+//     }
 //   }
 // }
