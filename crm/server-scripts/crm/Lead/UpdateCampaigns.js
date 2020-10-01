3b447a1621d2b7ea01fec20a57df05099967291e
Jože Fortun <joze.fortun@gmail.com>
2020-10-01 11:47:21 +0200

    Fix  undefined on campaign update

--- crm/server-scripts/crm/Lead/UpdateCampaigns.js
+++ crm/server-scripts/crm/Lead/UpdateCampaigns.js
@@ -10,48 +10,50 @@ export default {
   },
 
   async exec ({ $record, $oldRecord, $module, $namespace }, { Compose, ComposeAPI }) {
-    if ($record.values.CampaignId || $oldRecord.values.CampaignId) {
-      // Get unique campagin IDs, include old IDs so we can properly update campaigns
-      const campaignIDs = [
-        ...new Set([
-          ...$record.values.CampaignId || [],
-          ...$oldRecord.values.CampaignId || []
-        ])
-      ]
+    let campaignIDs = []
+    if ($record.values.CampaignId) {
+      campaignIDs.push(...$record.values.CampaignId)
+    }
+    if ($oldRecord && $oldRecord.values.CampaignId) {
+      campaignIDs.push(...$oldRecord.values.CampaignId)
+    }
 
-      // Make campaign filter
-      const campaignFilter = campaignIDs.map(cID => {
-        return `recordID = '${cID}'`
-      }).join(' OR ')
+    campaignIDs = [...new Set(campaignIDs)]
 
-      const reportFilter = {
-        namespaceID: $namespace.namespaceID,
-        moduleID: $module.moduleID,
-        dimensions: `DATE_FORMAT(created_at, '%Y-01-01')`,
-        metrics: '',
-      }
+    if (!campaignIDs.length) { return $record }
 
-      if (campaignFilter) {
-        await Compose.findRecords(campaignFilter, 'Campaigns')
-          .catch(() => ({ set: [] }))
-          .then(({ set }) => {
-            return Promise.all(set.map(async campaign => {
-              await ComposeAPI.recordReport({ ...reportFilter, filter: `CampaignId = '${campaign.recordID}'` })
-                .catch(() => ([]))
-                .then(response => {
-                  campaign.values.NumberOfLeads = response.reduce((a, b) => a + (b.count || 0), 0)
-                })
+    // Make campaign filter
+    const campaignFilter = campaignIDs.map(cID => {
+      return `recordID = '${cID}'`
+    }).join(' OR ')
 
-              await ComposeAPI.recordReport({ ...reportFilter, filter: `(CampaignId = '${campaign.recordID}') AND IsConverted` })
-                .catch(() => ([]))
-                .then(response => {
-                  campaign.values.NumberOfConvertedLeads = response.reduce((a, b) => a + (b.count || 0), 0)
-                })
+    const reportFilter = {
+      namespaceID: $namespace.namespaceID,
+      moduleID: $module.moduleID,
+      dimensions: `DATE_FORMAT(created_at, '%Y-01-01')`,
+      metrics: '',
+    }
 
-              return Compose.saveRecord(campaign)
-            }))
-          })
-      }
+    if (campaignFilter) {
+      await Compose.findRecords(campaignFilter, 'Campaigns')
+        .catch(() => ({ set: [] }))
+        .then(({ set }) => {
+          return Promise.all(set.map(async campaign => {
+            await ComposeAPI.recordReport({ ...reportFilter, filter: `CampaignId = '${campaign.recordID}'` })
+              .catch(() => ([]))
+              .then(response => {
+                campaign.values.NumberOfLeads = response.reduce((a, b) => a + (b.count || 0), 0)
+              })
+
+            await ComposeAPI.recordReport({ ...reportFilter, filter: `(CampaignId = '${campaign.recordID}') AND IsConverted` })
+              .catch(() => ([]))
+              .then(response => {
+                campaign.values.NumberOfConvertedLeads = response.reduce((a, b) => a + (b.count || 0), 0)
+              })
+
+            return Compose.saveRecord(campaign)
+          }))
+        })
     }
 
     return $record
--- crm/server-scripts/crm/Opportunity/UpdateCampaigns.js
+++ crm/server-scripts/crm/Opportunity/UpdateCampaigns.js
@@ -10,51 +10,53 @@ export default {
   },
 
   async exec ({ $record, $oldRecord, $module, $namespace }, { Compose, ComposeAPI }) {
-    if ($record.values.CampaignId || $oldRecord.values.CampaignId) {
-      // Get unique campagin IDs, include old IDs so we can properly update campaigns
-      const campaignIDs = [
-        ...new Set([
-          $record.values.CampaignId,
-          $oldRecord.values.CampaignId
-        ].filter(cID => cID))
-      ]
+    let campaignIDs = new Set()
+    if ($record.values.CampaignId) {
+      campaignIDs.add($record.values.CampaignId)
+    }
+    if ($oldRecord && $oldRecord.values.CampaignId) {
+      campaignIDs.add($record.values.CampaignId)
+    }
 
-      // Make campaign filter
-      const campaignFilter = campaignIDs.map(cID => {
-        return `recordID = '${cID}'`
-      }).join(' OR ')
+    campaignIDs = [...campaignIDs]
 
-      const reportFilter = {
-        namespaceID: $namespace.namespaceID,
-        moduleID: $module.moduleID,
-        dimensions: `DATE_FORMAT(created_at, '%Y-01-01')`,
-        metrics: 'sum(Amount) AS amount',
-      }
+    if (!campaignIDs.length) { return $record }
 
-      if (campaignFilter) {
-        await Compose.findRecords(campaignFilter, 'Campaigns')
-          .catch(() => ({ set: [] }))
-          .then(({ set }) => {
-            return Promise.all(set.map(async campaign => {
-              await ComposeAPI.recordReport({ ...reportFilter, filter: `CampaignId = '${campaign.recordID}'` })
-                .catch(() => ([]))
-                .then(response => {
-                  campaign.values.NumberOfOpportunities = response.reduce((a, b) => a + (b.count || 0), 0)
-                  campaign.values.AmountAllOpportunities = response.reduce((a, b) => a + (b.amount || 0), 0)
+    // Make campaign filter
+    const campaignFilter = campaignIDs.map(cID => {
+      return `recordID = '${cID}'`
+    }).join(' OR ')
 
-                })
+    const reportFilter = {
+      namespaceID: $namespace.namespaceID,
+      moduleID: $module.moduleID,
+      dimensions: `DATE_FORMAT(created_at, '%Y-01-01')`,
+      metrics: 'sum(Amount) AS amount',
+    }
 
-              await ComposeAPI.recordReport({ ...reportFilter, filter: `(CampaignId = '${campaign.recordID}') AND IsWon` })
-                .catch(() => ([]))
-                .then(response => {
-                  campaign.values.NumberOfWonOpportunities = response.reduce((a, b) => a + (b.count || 0), 0)
-                  campaign.values.AmountWonOpportunities = response.reduce((a, b) => a + (b.amount || 0), 0)
-                })
+    if (campaignFilter) {
+      await Compose.findRecords(campaignFilter, 'Campaigns')
+        .catch(() => ({ set: [] }))
+        .then(({ set }) => {
+          return Promise.all(set.map(async campaign => {
+            await ComposeAPI.recordReport({ ...reportFilter, filter: `CampaignId = '${campaign.recordID}'` })
+              .catch(() => ([]))
+              .then(response => {
+                campaign.values.NumberOfOpportunities = response.reduce((a, b) => a + (b.count || 0), 0)
+                campaign.values.AmountAllOpportunities = response.reduce((a, b) => a + (b.amount || 0), 0)
 
-              return Compose.saveRecord(campaign)
-            }))
-          })
-      }
+              })
+
+            await ComposeAPI.recordReport({ ...reportFilter, filter: `(CampaignId = '${campaign.recordID}') AND IsWon` })
+              .catch(() => ([]))
+              .then(response => {
+                campaign.values.NumberOfWonOpportunities = response.reduce((a, b) => a + (b.count || 0), 0)
+                campaign.values.AmountWonOpportunities = response.reduce((a, b) => a + (b.amount || 0), 0)
+              })
+
+            return Compose.saveRecord(campaign)
+          }))
+        })
     }
 
     return $record
